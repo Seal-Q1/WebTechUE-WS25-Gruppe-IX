ae26ab7d172e7822e535b30388ec4959f2d36cb8
Seal <ml.muellauer@aon.at>
2026-01-06 17:30:15 +0100

    Wipe Backend Clean for Reinstallation

--- backend/src/pool.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-
-let cfg = require('../config.json')
-
-let pool = new Pool(cfg.database);
-// Adapted to match the structure found here: https://node-postgres.com/features/connecting
-
-module.exports = pool;
