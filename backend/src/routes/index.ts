f410647e3d61736845aac480a8c44169f171f55b
Elias Pozewaunig <elias.pozewaunig@aau.at>
2026-02-15 07:15:07 +0100

    Renamed route more sensibly

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,5 @@
 export {default as usersRouter} from './users.routes';
-export {default as ordersRouter} from './orders.routes';
+export {default as ordersRouter} from './restaurant-orders.routes';
 export {default as orderPlacementRouter} from './order-placement.routes';
 export {default as menuItemsRouter} from './menu-items.routes';
 export {default as cuisinesRouter} from './cuisines.routes';
