0aca7d4ece82d1221582b9636ae6a17c7313cbcf
Elias Pozewaunig <elias.pozewaunig@aau.at>
2026-02-16 00:57:00 +0100

    Added filtering by reviews

--- frontend/src/app/customer/restarurants-overview/restaurant-grid/restaurant-grid.component.ts
+++ frontend/src/app/customer/restarurants-overview/restaurant-grid/restaurant-grid.component.ts
@@ -3,11 +3,14 @@ import {RestaurantService} from '../../../services/restaurant-service';
 import {RestaurantGridElement} from '../restaurant-grid-element/restaurant-grid-element';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {RestaurantReviewAggregateDto} from '@shared/types';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faFilter, faStar} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-restaurant-grid',
   imports: [
-    RestaurantGridElement
+    RestaurantGridElement,
+    FaIconComponent
   ],
   templateUrl: './restaurant-grid.component.html',
   styleUrl: './restaurant-grid.component.css',
@@ -18,18 +21,58 @@ export class RestaurantGrid {
   restaurants = toSignal(this.restaurantService.getAllRestaurants(), { initialValue: [] });
   restaurantRatings = toSignal(this.restaurantService.getAggregatedReviews(), { initialValue: [] });
 
+  restrIdToRatingMap = computed(() => {
+    const map: Map<number, RestaurantReviewAggregateDto> = new Map();
+
+    for (const rating of this.restaurantRatings()) {
+      map.set(rating.restaurantId, rating);
+    }
+
+    for (const restaurant of this.restaurants()) {
+      if(!map.has(restaurant.id)) {
+        const noReview: RestaurantReviewAggregateDto = {
+          restaurantId: restaurant.id,
+          avg: 0,
+          count: 0
+        }
+        map.set(restaurant.id, noReview);
+      }
+    }
+    return map;
+  });
+
+  advancedFilters = signal(false);
   nameSearchTerm = signal('');
+  minStars = signal(0);
 
   filteredRestaurants = computed(() => {
     const searchTerm = this.nameSearchTerm().toLowerCase();
-    return this.restaurants().filter((restaurant) => {
-      return restaurant.name.toLowerCase().includes(searchTerm);
-    });
+    return this.restaurants()
+      .filter((restaurant) => {
+        return restaurant.name.toLowerCase().includes(searchTerm);
+      })
+      .filter((restaurant) => {
+        const rating = this.restrIdToRatingMap().get(restaurant.id)!;
+        return rating.avg >= this.minStars();
+      });
   })
+
+  toggleAdvancedFilters() {
+    this.advancedFilters.set(!this.advancedFilters());
+  }
 
   onNameSearch(e: Event) {
     const target = e.target as HTMLInputElement;
     this.nameSearchTerm.set(target.value);
+  }
+
+  onMinStarsSet(e: Event) {
+    const target = e.target as HTMLInputElement;
+    let value = parseInt(target.value);
+    if(isNaN(value)) {
+      value = 0;
+    }
+    this.minStars.set(value);
   }
 
   getRestaurantRating(restaurantId: number) {
@@ -45,4 +88,7 @@ export class RestaurantGrid {
     }
     return noRating;
   }
+
+  protected readonly faFilter = faFilter;
+  protected readonly faStar = faStar;
 }
