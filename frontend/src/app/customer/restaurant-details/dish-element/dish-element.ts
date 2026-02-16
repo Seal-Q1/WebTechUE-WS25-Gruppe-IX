31a9be1d39856c7d9717ec25861b95635d10a529
Elias Pozewaunig <elias.pozewaunig@aau.at>
2026-02-16 23:19:53 +0100

    Fixed event propagation in dish overview

--- frontend/src/app/customer/restaurant-details/dish-element/dish-element.ts
+++ frontend/src/app/customer/restaurant-details/dish-element/dish-element.ts
@@ -35,7 +35,8 @@ export class DishGridElement {
     { initialValue: null }
   );
 
-  onAddToCart() {
+  onAddToCart(event: Event) {
+    event.stopPropagation();
     let currentQuantity = this.cartService.getItemQuantity(this.dish())
     this.cartService.setCartEntry(this.dish(), currentQuantity + 1)
   }
