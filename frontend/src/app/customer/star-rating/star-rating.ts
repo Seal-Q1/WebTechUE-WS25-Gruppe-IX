4ae8f5227bc546877451047ab2831b5d344a181d
Elias Pozewaunig <elias.pozewaunig@aau.at>
2026-02-16 23:11:18 +0100

    Improved rating styling

--- frontend/src/app/customer/star-rating/star-rating.ts
+++ frontend/src/app/customer/star-rating/star-rating.ts
@@ -1,14 +1,20 @@
 import {Component, Input} from '@angular/core';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
-import {faStar} from '@fortawesome/free-solid-svg-icons';
+import {faStar, faStar as fasStar} from '@fortawesome/free-solid-svg-icons';
+import {faStar as farStar} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-star-rating',
-  imports: [],
+  imports: [
+    FaIconComponent
+  ],
   templateUrl: './star-rating.html',
   styleUrl: './star-rating.css',
 })
 export class StarRating {
   @Input() rating = 0;
   readonly starArray = [1, 2, 3, 4, 5];
+
+  protected readonly fasStar = fasStar;
+  protected readonly farStar = farStar;
 }
